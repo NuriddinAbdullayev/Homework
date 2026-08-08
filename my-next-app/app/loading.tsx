dc82ca716faee0e50a50644a33e412d7dee1bb7e
Nuriddin Abdullayev <nuriddinabdullayev0722@gmail.com>
2026-08-08 17:23:11 +0500

    modified

--- my-next-app/app/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '200px' }}>
-      <h2>Yuklanmoqda...</h2>
-    </div>
-  );
-}
--- my-next-app/app/users/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function UsersPage() {
-  return (
-    <div>
-      <h1>Users List Page</h1>
-    </div>
-  );
-}
